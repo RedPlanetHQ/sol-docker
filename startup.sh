fcd67a8c275e9947f4383075b0d02d2951e6225d
Manoj K <saimanoj58@gmail.com>
2025-04-22 22:20:31 +0530

    init: sigma docker compose

---
+++ startup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Check if the docker directory exists
+if [ -d "./docker" ]; then
+    echo "Removing existing docker directory..."
+    rm -rf ./docker
+fi
+
+# Clone the repository
+git clone https://github.com/tegonhq/sigma-docker.git
+
+# Fetch the .env file from Secret Manager
+gcloud secrets versions access latest --secret=sigma-prod > docker/.env
+
+mkdir -p ./docker/certs
+gcloud secrets versions access latest --secret=tegon-gcs > docker/certs/tegon-gcs.json
+
+
+# Navigate to the app directory
+cd ./docker
+
+# Read the VERSION from the .env file
+export VERSION=$(grep -oP '(?<=VERSION=).*' .env)
+
+
+docker pull tegonhq/sigma-server:$VERSION
+docker pull tegonhq/sigma-webapp:$VERSION
+
+docker compose up -d
