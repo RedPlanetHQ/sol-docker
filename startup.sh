687bea4f405eeb781ed7b80cbf5e529c969e37fa
Manoj K <saimanoj58@gmail.com>
2025-06-18 09:34:23 +0530

    Update environment variables and container names for sol deployment

--- startup.sh
+++ startup.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
+sudo mkdir -p /efs
+sudo mount -t efs fs-0638e1b02b250066b:/ /efs
+
+sudo mkdir -p /efs/qdrant
+sudo chmod 777 /efs/qdrant 
+
 # Check if the docker directory exists
 if [ -d "./docker" ]; then
-    echo "Removing existing docker directory..."
-    rm -rf ./docker
+    echo "Removing existing core directory..."
+    sudo rm -rf ./docker
 fi
 
 # Clone the repository
-git clone https://github.com/tegonhq/sigma-docker.git docker
+git clone https://github.com/RedPlanetHQ/sol-docker.git docker
 
-# Fetch the .env file from Secret Manager
-gcloud secrets versions access latest --secret=sigma-prod > docker/.env
+# Make sure you have permission to write to it
+sudo chown $(whoami) docker
+sudo chmod 755 docker
 
-mkdir -p ./docker/certs
-gcloud secrets versions access latest --secret=tegon-gcs > docker/certs/tegon-gcs.json
-
+# Fetch the .env file from AWS Secrets Manager
+aws secretsmanager get-secret-value --secret-id sol-prod --query SecretString --output text > docker/.env
 
 # Navigate to the app directory
 cd ./docker
 
-# Read the VERSION from the .env file
-export VERSION=$(grep -oP '(?<=VERSION=).*' .env)
-
-
-docker pull tegonhq/sigma-server:$VERSION
-docker pull tegonhq/sigma-webapp:$VERSION
-
+# Start Docker containers
 docker compose up -d
